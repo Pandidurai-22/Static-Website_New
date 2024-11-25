2435a5c699cc39de8143b53cc05e9158bffb3a24
Pandidurai-S <32mins.who@gmail.com>
2024-11-25 14:45:30 +0530

    hello user

--- jenkins/script/build.sh
+++ jenkins/script/build.sh
@@ -4,6 +4,6 @@ cd "/c/Users/HP/Desktop/javascript basic projects/day 2(analog clock)/Static-web
 
 docker build -t static-wesbite:latest .
 
-docker run -d -p 8081:80 static-website:latest
+docker run -d -p 8081:80 static-wesbite:latest
 
 docker ps
